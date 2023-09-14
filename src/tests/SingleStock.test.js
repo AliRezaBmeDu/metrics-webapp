5f0ff1159a7847d9e6d611b977991e2265abe293
AliReza <ali.reza.buet@gmail.com>
2023-09-14 17:34:56 +0600

    Complete the test for the Single Stock component

--- src/tests/SingleStock.test.js
+++ src/tests/SingleStock.test.js
@@ -1,21 +1,82 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
 import SingleStock from '../components/SingleStock';
-import store from '../redux/store';
-import '@testing-library/jest-dom';
-import configureMockStore from 'redux-mock-store';
 
-describe('Stock details: ', () => {
-  it('Should render all the components of company details', () => {
+// Mock Redux store
+const mockStore = configureStore([]);
+
+describe('SingleStock Component', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      stocks: {
+        stockDetail: {
+          companyName: 'Test Company',
+          image: 'test-image-url',
+          mktCap: 'Test Market Cap',
+          price: 'Test Price',
+          volAvg: 'Test Volume Average',
+          sector: 'Test Sector',
+          ceo: 'Test CEO',
+          website: 'http://example.com',
+        },
+        loadingDetail: false,
+      },
+    });
+
+    // Mock the getStockDetail action
+    store.dispatch = jest.fn();
+  });
+
+  it('renders SingleStock component with stock details', async () => {
     render(
       <Provider store={store}>
-        <BrowserRouter>
-          <SingleStock />
-        </BrowserRouter>
-      </Provider>,
+        <MemoryRouter initialEntries={['/stocks/TEST']}>
+            <SingleStock />
+        </MemoryRouter>
+      </Provider>
     );
-    const stockDetails = screen.getByTestId('stockDetail');
-    expect(stockDetails).toBeInTheDocument();
+
+    // Wait for the component to load
+    await waitFor(() => {
+      expect(screen.getByText('Company Name')).toBeInTheDocument();
+      expect(screen.getByText('Test Company')).toBeInTheDocument();
+    });
   });
+
+  it('renders loading message when loadingDetail is true', () => {
+    store = mockStore({
+      stocks: {
+        stockDetail: {
+          companyName: 'Test Company',
+          image: 'test-image-url',
+          mktCap: 'Test Market Cap',
+          price: 'Test Price',
+          volAvg: 'Test Volume Average',
+          sector: 'Test Sector',
+          ceo: 'Test CEO',
+          website: 'http://example.com',
+        },
+        loadingDetail: true,
+      },
+    });
+
+    // Mock the getStockDetail action
+    store.dispatch = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/stocks/TEST']}>
+            <SingleStock />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByTestId('stockDetail')).toHaveTextContent('Loading...please wait');
+  });
+
 });
